2f1e7c3a6f8d834ead64c70e29461f264de57e6d
racha ouldbabaali <rachaould@gmail.com>
2023-05-09 20:12:03 +0100

    Add task 3 and 4

--- testing.test.js
+++ testing.test.js
@@ -1,33 +1,42 @@
 const stringLength = require('./StringLength');
 const reverseString = require('./ReverseString');
 const calculator = require('./Calculator');
+const capitalize = require('./Capitalize');
 
-test('stringLength("Hello") should return 5', () => {
-    expect(stringLength("Hello")).toBe(5);
-}
-);
+describe ('stringLength', () => {
+    test('stringLength("Hello") should return 5', () => {
+        expect(stringLength("Hello")).toBe(5);
+    }
+    );
 
-test('stringLength("") should throw an error', () => {
-    expect(() => stringLength("")).toThrow();
-}
-);
+    test('stringLength("") should throw an error', () => {
+        expect(() => stringLength("")).toThrow("Input string must be at least 1 character long.");
+    }
+    );
 
-test('stringLength("Hello World") should throw an error', () => {
-    expect(() => stringLength("Hello World")).toThrow();
-}
-);
+    test('stringLength("Hello World!") should throw an error', () => {
+        expect(() => stringLength("Hello World!")).toThrow("Input string must not be longer than 10 characters.");
+    }
+    );
 
-test('reverseString("Hello") should return "olleH"', () => {
-    expect(reverseString("Hello")).toBe("olleH");
-}
-);
+});
 
-test('reverseString("") should return ""', () => {
-    expect(reverseString("")).toBe("");
-}
-);
+describe ('reverseString', () => {
+    test('reverseString("Hello") should return "olleH"', () => {
+        expect(reverseString("Hello")).toBe("olleH");
+    }
+    );
 
+    test('reverseString("") should return ""', () => {
+        expect(reverseString("")).toBe("");
+    }
+    );
 
+    test('reverseString("Hello World!") should return "!dlroW olleH"', () => {
+        expect(reverseString("Hello World!")).toBe("!dlroW olleH");
+    }
+    );
+});
 
 describe ('calculator', () => {
     test('add(1, 2) should return 3', () => {
@@ -44,7 +53,34 @@ describe ('calculator', () => {
         expect(calculator.divide(1, 2)).toBe(0.5);
     }
     );
-    
+
+    test('multiply(1, 2) should return 2', () => {
+        expect(calculator.multiply(1, 2)).toBe(2);
+    }
+    );
+
 }
 );
 
+describe ('capitalize', () => {
+    test('capitalize("hello") should return "Hello"', () => {
+        expect(capitalize("hello")).toBe("Hello");
+    }
+    );
+
+    test('capitalize("hello world!") should return "Hello world!"', () => {
+        expect(capitalize("hello world!")).toBe("Hello world!");
+    }
+    );
+
+    test('capitalize("123") should return "123"', () => {
+        expect(capitalize("123")).toBe("123");
+    }
+    );
+
+    test('capitalize("") should return ""', () => {
+        expect(capitalize("")).toBe("");
+    }
+    );
+
+});
